e6bff69c3fb480d72c992f00499ab30f89c7de7b
tpedrozo <mr.thiagocesar@hotmail.com>
2025-03-31 09:57:08 -0300

    Update CORS configuration in main.ts to use full URL for 4me-new-quiz.vercel.app

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '4me-new-quiz.vercel.app',
+    origin: 'https://4me-new-quiz.vercel.app',
     credentials: true,
   });
   app.useGlobalPipes(
