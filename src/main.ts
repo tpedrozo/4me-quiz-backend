76c0f7c1870535c50846f26e9011cf213bd17692
tpedrozo <mr.thiagocesar@hotmail.com>
2025-03-31 09:49:37 -0300

    Update CORS configuration in main.ts to allow requests from '4me-new-quiz.vercel.app' with credentials enabled

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '4me-new-quiz.vercel.app',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
